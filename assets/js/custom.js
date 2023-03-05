f104bca9f8377e973a95821e9688fb7a72383e61
yshu17 <magnificojack@gmail.com>
2023-03-05 17:47:18 +0300

    upd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,10 @@
-
 var portal = 'it';
 var theme = '3-Samsung_S10';
 var template = 'surbox';
 var brand_country = 'Spain';
 var dayNames = Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
 var monthNames = new Array("Janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
-var img_regalo = "./assets/images/img-2.png";
+var img_regalo = "img-2.png";
 var minutos_y = "minutos e ";
 var segundos = "segundos ";
 var url_f = "";
@@ -14,7 +13,7 @@ var modalOptions = {
     keyboard: false
 };
 var adS = ["bG9jYWwucHJlbG9hZGVycy5jb20=", "bG9jYWwucHJlbG9hZGVycy5jb20="];
-//<!--Facebook Pixel Code-->
+
 !function (f, b, e, v, n, t, s) {
     if (f.fbq) return;
     n = f.fbq = function () { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments) };
@@ -29,6 +28,6 @@ var adS = ["bG9jYWwucHJlbG9hZGVycy5jb20=", "bG9jYWwucHJlbG9hZGVycy5jb20="];
     s = b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t, s)
 }
-(window, document, 'script', '//connect.facebook.net/en_US/fbevents.js');
+    (window, document, 'script', '//connect.facebook.net/en_US/fbevents.js');
 fbq('init', '{lander.param:fbpixel}');
-fbq('track', 'PageView');
+fbq('track', "PageView");
